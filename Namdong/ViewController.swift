852ef8ae3edc9af3e6a096a2f72a66a27abc5427
Chris Song <purechris@me.com>
2017-07-31 22:43:43 +0900

    Initial Commit

---
+++ Namdong/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Namdong
+//
+//  Created by Chris Song on 2017. 7. 31..
+//  Copyright © 2017년 Chris Song. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
