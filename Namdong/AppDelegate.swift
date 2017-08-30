fa491c9f5e1587b5852b4630a935269fddb938a5
Chris Song <chris@healcerion.com>
2017-08-30 15:20:33 +0900

    Save webview cookies

--- Namdong/AppDelegate.swift
+++ Namdong/AppDelegate.swift
@@ -12,6 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    let keyCookie = "Cookie"
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
@@ -27,6 +28,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        // save cookie to keychain
+        let cookies = HTTPCookieStorage.shared.cookies
+        if let cookieCount = cookies?.count, cookieCount > 0 {
+            let cookieData = NSKeyedArchiver.archivedData(withRootObject: cookies as Any)
+            UserDefaults.standard.set(cookieData, forKey: keyCookie)
+        }
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -35,6 +42,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        // load cookie from keychain
+        let cookiesData = UserDefaults.standard.object(forKey: keyCookie)
+        if cookiesData != nil {
+            let cookies = NSKeyedUnarchiver.unarchiveObject(with: cookiesData as! Data)
+            for cookie in cookies as! [HTTPCookie] {
+                HTTPCookieStorage.shared.setCookie(cookie)
+            }
+        }
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
